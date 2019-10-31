ac108fc903bf03ec16228b10d0274193aba46cf1
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-10-30 22:19:21 -0700

    Add SearchController class and word-search.jsp

---
+++ WebContent/WEB-INF/view/word-search.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	
+	<body>
+		<h1>Bible-it Word Search</h1>
+	</body>
+
+</html>
---
+++ src/co/bibleit/bibleitspring/SearchController.java
@@ -0,0 +1,14 @@
+package co.bibleit.bibleitspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// this class is suppose to replicate the HomeController class
+@Controller
+public class SearchController {
+	
+	@RequestMapping("/")
+	public String showPage() {
+		return "search-word";
+	}
+}
