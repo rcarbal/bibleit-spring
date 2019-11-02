3e0aef77043e773496310b46a39781710a53e950
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-11-01 23:32:34 -0700

    Add data to model for  SearchController processSearch

--- src/co/bibleit/bibleitspring/SearchController.java
+++ src/co/bibleit/bibleitspring/SearchController.java
@@ -1,6 +1,9 @@
 package co.bibleit.bibleitspring;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // this class is suppose to replicate the HomeController class
@@ -13,7 +16,17 @@ public class SearchController {
 	}
 	
 	@RequestMapping("/processSearch")
-	public String processSerch() {
+	public String processSerch(HttpServletRequest request, Model model) {
+		// read the request parameter form the HTML form
+		String data = request.getParameter("search");
+		
+		// Create data that is being parsed 
+		String result = "There is no results for " + data;
+
+		// Add the message to the model
+		model.addAttribute("result", result);
+		
 		return "word-search-results";
 	}
+	
 }
